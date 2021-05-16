c228e284d778e8b95a2afc5362cc576e264dd0a1
hoffmann <silas.hoffmann@dps.de>
2021-05-16 14:26:03 +0200

    added spring api and docker compose

---
+++ mdb-project/src/main/java/dps/hoffmann/mdb_project/HelloApplication.java
@@ -0,0 +1,9 @@
+package dps.hoffmann.mdb_project;
+
+import javax.ws.rs.ApplicationPath;
+import javax.ws.rs.core.Application;
+
+@ApplicationPath("/api")
+public class HelloApplication extends Application {
+
+}
